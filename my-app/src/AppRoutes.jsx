c81d20d93f2d6caea9eafd2f3cca078cdcf4fd0e
Talescruzs <tcs2017@hotmail.com>
2023-01-29 18:21:09 -0300

    commit app router

--- my-app/src/AppRoutes.jsx
+++ my-app/src/AppRoutes.jsx
@@ -1,65 +1,21 @@
-import React, { useContext } from 'react'
-
+import React from 'react'
 import{
     BrowserRouter as Router,
     Route,
     Routes,
-    Navigate,
 } from "react-router-dom"
-
+import {
+    Private,
+    RedirectType,
+    PrivAluno,
+    PrivProf,
+} from "./AppRules"
+import { AuthProvider } from "./contexts/auth"
 import RegisterPage from "./pages/login/register"
 import AlunoPage from "./pages/aluno/alunoPage"
 import ProfPage from "./pages/professor/profPage"
-import { AuthProvider, AuthContext } from "./contexts/auth"
 
 const AppRoutes=()=>{
-    const Private=({ children })=>{
-        const { authenticated, loading } = useContext(AuthContext)
-        if(loading){
-            return(
-                <div className='loading'>carregando</div>
-            )
-        }
-        
-        if(!authenticated){
-            return(
-                <Navigate to="/registrar"/>
-            )
-        }
-        return children
-    }
-    const RedirectType=()=>{
-        const {user} = useContext(AuthContext)
-        if(user.type==="Aluno"){
-            return(
-                <Navigate to="/aluno"/>
-            )
-        }
-        if(user.type==="Professor"){
-            return(
-                <Navigate to="/professor"/>
-            )
-        }
-    }
-    const PrivAluno=({ children })=>{
-        const {user} = useContext(AuthContext)
-        if(user.type==="Professor"){
-            return(
-                <Navigate to="/professor"/>
-            )
-        }
-        return children
-    }
-    const PrivProf=({ children })=>{
-        const {user} = useContext(AuthContext)
-        if(user.type==="Aluno"){
-            return(
-                <Navigate to="/aluno"/>
-            )
-        }
-        return children
-    }
-
 
     return(
         <Router>
@@ -67,8 +23,20 @@ const AppRoutes=()=>{
                 <Routes>
                     <Route exact path="/" element={<Private><RedirectType/></Private>} />
                     <Route exact path="/registrar" element={<RegisterPage/>} />
-                    <Route exact path="/aluno" element={<Private><PrivAluno><AlunoPage/></PrivAluno></Private>} />
-                    <Route exact path="/professor" element={<Private><PrivProf><ProfPage/></PrivProf></Private>} />
+                    <Route exact path="/aluno" element={
+                        <Private>
+                            <PrivAluno>
+                                <AlunoPage/>
+                            </PrivAluno>
+                        </Private>} 
+                    />
+                    <Route exact path="/professor" element={
+                        <Private>
+                            <PrivProf>
+                                <ProfPage/>
+                            </PrivProf>
+                        </Private>} 
+                    />
                 </Routes>
             </AuthProvider>
         </Router>
